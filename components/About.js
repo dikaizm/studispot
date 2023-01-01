af6c03530aaa0e0147dc45a2d81a552b4b344447
dikaizm <dikamaah@gmail.com>
2023-01-01 21:46:07 +0700

    fix responsive

--- components/About.js
+++ components/About.js
@@ -5,7 +5,7 @@ export default function About() {
                     <img className="max-width-full" src="/about-us.svg" />
                 <div className="px-5 pt-5 md:px-0 md:pt-0">
                     <h2 className="max-w-[220px] md:max-w-lg text-spotBlue text-2xl md:text-3xl font-bold">Kenalan dengan Studispot, yuk!</h2>
-                    <p className="max-w-lg text-md md:text-base text-slate-600 pt-6 md:pt-10">
+                    <p className="max-w-lg text-sm md:text-base text-slate-600 pt-6 md:pt-10">
                         <span className="leading-loose">Studispot membantu pembelajar untuk memperluas pengetahuan dan mengembangkan keterampilan mereka, dengan memberikan akses ke komunitas pembelajar yang positif dan terbuka.</span><br /><br />
                     
                         <span className="font-bold leading-loose">Dengan Studispot, pembelajar dapat belajar dengan lebih efektif dan menyenangkan dalam satu platform yang komprehensif.</span>
